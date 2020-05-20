5a6b592c933ce9e8b65004f5ab2aff4f878f9a5e
Giovanni <g.l.mierop@gmail.com>
2020-05-20 10:44:44 +0200

    First commit

---
+++ view/employee/index.php
@@ -0,0 +1,24 @@
+<?php
+// maak een overzicht lijst van ALLE personen
+$employees = "";
+if (isset($data)) {
+    $employees = $data;
+}
+?>
+<h1>Overzicht van personen</h1>
+<ul>
+    <?php foreach ($employees as $employee) { ?>
+        <li>
+            <!--<span>Karel is 12 jaar</span>-->
+            <?php
+            // de opbouw van de link bepaald welke method in welke controller aangeroepen wordt.
+            // het woordje "employee" in de url betekent dat het framework moet zoeken naar een controller genaamd "EmployeeController".
+            // Hij maakt van de eerste letter een hoofdletter en plakt er zelf "Controller" achter.
+            // Het woordje "update" of "delete" betekent dat hij in deze controller moet zoeken naar een method met deze naam.
+            $employeeString = $employee['name'] . " is " . $employee['age'] . " jaar.";
+            ?>
+            <span><?= $employeeString ?></span>
+            <a href="<?=URL?>employee/update/<?= $employee['id'] ?>">Wijzigen</a> <a href="<?=URL?>employee/delete/<?= $employee['id'] ?>">Verwijderen</a>
+        </li>
+    <?php } ?>
+</ul>
